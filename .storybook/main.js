e4b5cb585e54db0a092e90aac05580ebc9a85b66
SasLord <75478678+SasLord@users.noreply.github.com>
2021-01-06 01:23:08 +0300

    Message SB change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
   "stories": [
+    "../src/components/**/*.stories.js",
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls"
   ]
 }
--- src/App.svelte
+++ src/App.svelte
@@ -2,32 +2,38 @@
 	import Message from './components/Message.svelte';
 	import { beforeUpdate, afterUpdate } from 'svelte';
 
+	export let theme;
+	theme = 'Transperent';
+
 	let messages = [{
 		avatar: 'https://platform.exhale24.ru/images/photo-1.png',
 		name: 'Александр Алексеенко',
-		tags: [],
+		tag: '',
 		text: 'ну да, на клиенте все равно мы забиваем на это всё пока. CoreException/CoreError - а где это посмотреть, что-то по коду не найду такого? Или это nodejs`ное что-то?',
-		date: new Date //'10:15'
+		date: new Date(2020, 11, 28, 10, 15),
+		theme: ''
 	}, {
 		avatar: 'https://platform.exhale24.ru/images/photo-2.png',
 		name: 'Андрей Платов',
-		tags: [],
+		tag: '',
 		text: '<p>наше</p><p>хм походу после всех переписываний исчело за неиспользованием</p><p>но думаю появится когда будем четче обрабатывать <a class="link-aSide-1" href="#">Ошибки</a></p>',
-		date: new Date //'10:31'
+		date: new Date(2020, 11, 28, 10, 31),
+		theme: ''
 	}, {
 		avatar: '',
 		name: 'Сергей Буевич',
-		tags: [],
+		tag: '',
 		text: `<p>в rpc.ts объявлен</p><pre>export interface RpcError {
   code: number
   message?: string
   data?: any
 }</pre>
 <p>Надо бы придумать, что за code numbers там будут для нотификаций клиента об ошибках сервера. Сделал, чтобы сервер все эксепшны заворачивал в еррор респонз с этим RpcError'ом, тока код непонятно какой ставить )</p>`,
-		date: new Date //'12:30'
+		date: new Date(2020, 11, 28, 12, 30),
+		theme: ''
 	}];
 
-	let value;
+	let value = '';
 
 	let divChat;
 	let autoscroll;
@@ -42,22 +48,33 @@
 		messages = [...messages, {
 			avatar: '',
 			name: 'Local',
-			tags: [],
+			tag: '',
 			text: value,
-			date: new Date
+			date: new Date,
+			theme: theme
 		}];
 		value = '';
 	}
+	function changeTheme () {
+		messages.map((msg) => msg.theme = theme );
+		messages = messages;
+	}
+	changeTheme();
 </script>
 
 <div class="container">
 <div class="chatBox" bind:this={divChat}>
-	{#each messages as message, i}
+	{#each messages as message, i (message)}
 		<Message {message}/>
 	{/each}
 </div>
 <textarea class="chatMsg" name="newMessage" bind:value="{value}"></textarea>
 <button class="chatMsg" on:click|preventDefault={addMsg} type="submit">Добавить</button>
+<select bind:value={theme} on:change={changeTheme}>
+	<option value="Transperent">Transperent</option>
+	<option value="Biege">Biege</option>
+	<option value="Black">Black</option>
+</select>
 </div>
 
 <style>
